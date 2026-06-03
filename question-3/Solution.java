d0f7ef88a93849eeaee01eb98c7c293ead7a31de
muradm9 <muradm9@nycstudents.net>
2026-06-02 23:18:15 -0400

    Update Solution.java
    Added moreHistoryThanMathAbsences() method which returns the amount of people who have more history than math absences

--- question-3/Solution.java
+++ question-3/Solution.java
@@ -1,1 +1,14 @@
 public class Solution
+{
+    public int moreHistoryThanMathAbsences() {
+        int result = 0;
+        for (CourseRecord sH : historyList) {
+            for (CourseRecord sM : mathList) {
+                if (sM.getStudentID().equals(sH.getStudentID()) && (sH.getAbsences() > sM.getAbsences()) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
