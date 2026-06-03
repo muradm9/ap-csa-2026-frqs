369a2b35b68a71ae8ac19d37f5a55a3be94eb0a2
muradm9 <muradm9@nycstudents.net>
2026-06-02 23:20:10 -0400

    Update Bottle.java
    Added implementation of class Bottle

--- question-2/Bottle.java
+++ question-2/Bottle.java
@@ -1,1 +1,19 @@
 public class Bottle
+{
+    private double volume;
+    private double currentAmount;
+
+    public Bottle(double v) {
+        volume = v;
+        currentAmount = v;
+    }
+
+    public double updateAmount(double minus) {
+        double temp = volume * 0.25;
+        currentAmount -= minus;
+        if (currentAmount < temp) {
+            currentAmount = volume;
+        }
+        return currentAmount;
+    }
+}
