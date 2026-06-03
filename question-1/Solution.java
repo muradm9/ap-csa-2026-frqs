82dc87e2e058254a9523e17fa1d122f93b26248f
muradm9 <muradm9@nycstudents.net>
2026-06-02 23:01:22 -0400

    Update Solution.java
    Add Account method which returns name that is available

--- question-1/Solution.java
+++ question-1/Solution.java
@@ -1,1 +1,12 @@
 public class Solution
+{
+    public Account(String requestedName) {
+        String result = requestedName;
+        int i = 0;
+        while (!isAvailable(result)) {
+            result = result + i;
+            ++i;
+        }
+        username = result;
+    }
+}
