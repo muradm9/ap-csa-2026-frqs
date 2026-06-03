f3e3dce652870b74813222138739d4755b608652
muradm9 <muradm9@nycstudents.net>
2026-06-02 23:12:46 -0400

    Update Solution.java
    Added getPointsForRow() method which returns score based on the colors from the board

--- question-4/Solution.java
+++ question-4/Solution.java
@@ -1,1 +1,22 @@
 public class Solution
+{
+    public int getPointsForRow(int targetRow) {
+        Space[] row = board[targetRow];
+        boolean sameColor = true;
+        int score = row[0].getPoints();
+        String color = row.getColor();
+
+        for (int i = 1; i < row.length; i++) {
+            if (!color.equals(row[i].getColor())) {
+                sameColor = false;
+            }
+            score += row[i].getPoints();
+        }
+
+        if (sameColor) {
+            score *= 2;
+        }
+
+        return score;
+    }
+}
